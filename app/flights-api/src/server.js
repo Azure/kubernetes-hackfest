796bb500f63591620fa6e3f358eddee61db5f1d8
Ray Kao <ray@peopleandcode.com>
2018-07-10 18:14:29 -0400

    basics of the flight api with restify

---
+++ app/flights-api/src/server.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch');
+const restify = require('restify');
+const port = process.env.PORT || 3000;
+
+const server = restify.createServer();
+
+
+server.get('/healthprobe', function(req, res, next){
+  res.send("I'm healthy");
+  next();
+});
+
+server.get('/readiness', function(req, res, next){
+  res.send("I'm Ready");
+  next();
+});
+
+server.listen(port, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
