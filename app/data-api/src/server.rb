432e74e538d89f7f9a79e367dec77ee02d342750
Ray Kao <ray@peopleandcode.com>
2018-07-10 16:54:42 -0400

    added basic data connection

--- app/data-api/src/server.rb
+++ app/data-api/src/server.rb
@@ -1,2 +1,36 @@
 require 'sinatra'
+require "sinatra/reloader" if :development?
+require 'mongo'
 
+DBURI = ENV["MONGOURI"]
+DBOptions = {
+  user: ENV["MONGOUSER"],
+  password: ENV["MONGOPWD"],
+  database: ENV["MONGODB"],
+  ssl: true
+}
+
+begin
+  DBClient = Mongo::Client.new([DBURI], DBOptions)
+  puts('Client Connection: ')
+  puts(DBClient.cluster.inspect)
+  puts
+  puts('Collection Names: ')
+  puts(DBClient.database.collection_names.inspect)
+  puts('Connected!')
+rescue StandardError => err
+  puts('Error: ')
+  puts(err)
+end
+
+get '/' do
+  flights = DBClient[:flights].find()
+end
+
+get '/healthprobe' do
+  "Put this in your pipe and smoke it!"
+end
+
+get 'readinessprobe' do
+  "Ready to go!"
+end
