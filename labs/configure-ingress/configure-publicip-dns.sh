5a8a9076938d07a3b44814c37b8426c1e23f3056
Kevin Harris <kev@spiderman.local>
2018-07-10 16:47:09 -0400

    Initial Networking and Security sections.

---
+++ labs/configure-ingress/configure-publicip-dns.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Public IP address
+IP="40.121.18.237"
+
+# Name to associate with public IP address
+DNSNAME="<USER-INITIALS>ingress"
+
+# Get the resource-id of the public ip
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public ip address with dns name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
