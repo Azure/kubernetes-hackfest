2ec61999e5c906f73317b4c2fd35444cbb226ac1
Ray Kao <ray@peopleandcode.com>
2018-07-11 09:39:25 -0400

    added context/comments and included env HOSTPORT values

--- app/data-api/src/server.rb
+++ app/data-api/src/server.rb
@@ -2,20 +2,25 @@ require "sinatra"
 require "sinatra/reloader" if :development?
 require "mongo"
 
+## Database values
 DBURI = ENV["MONGOURI"]
-
 DBOptions = {}
 DBOptions[:user] = ENV["MONGOUSER"] 
 DBOptions[:password] = ENV["MONGOPWD"] 
 DBOptions[:database] = ENV["MONGODB"] 
-
  # Note to self: bash "MONGODBSSL" value is only false if it is completely *NOT* set i.e. bash variable does not exist.  
  # Otherwise even a value of false returns true in ruby when grabbing from ENV ¯\_(ツ)_/¯
 DBOptions[:ssl] = true if ENV["MONGODBSSL"]
 
-HOSTADDRESS = ENV["HOSTADDRESS"] || "0.0.0.0"
+# Cache Service values
 
+# Server values
+HOSTADDRESS = ENV["HOSTADDRESS"] || "0.0.0.0"
+HOSTPORT = ENV["HOSTPORT"] || "4567"
+
+# Set Server Values
 set :bind, HOSTADDRESS
+set :port, HOSTPORT
 set :server, "thin"
 
 begin
