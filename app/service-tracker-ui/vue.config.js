01537b387e052e8492053f59af120a02c192f4c6
Joey Schluchter <jschluchter@gmail.com>
2018-09-01 01:06:24 -0400

    update config again

--- app/service-tracker-ui/vue.config.js
+++ app/service-tracker-ui/vue.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
  lintOnSave: false,
   devServer: {
-    // show variables at http://localhost:8080/variables
+    // show variables when running http://localhost:8080/variables
     before: function(app) {
       app.get('/variables', (req, res) => {
         var currentEnv = { 
