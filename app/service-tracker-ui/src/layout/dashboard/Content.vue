84d63edd3baf865303ca9b69e446ca1a656b9b5f
Joey Schluchter <jschluchter@gmail.com>
2018-09-12 14:19:16 -0700

    update data dockerfiles

--- app/service-tracker-ui/src/layout/dashboard/Content.vue
+++ app/service-tracker-ui/src/layout/dashboard/Content.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <div class="container-fluid">
         <transition name="fade" mode="out-in">
-          <!-- your content here -->
+
           <router-view></router-view>
         </transition>
     </div>
