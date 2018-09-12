fe5d15e7d37aa9c1f9e4a633362f97eccbd75be3
Brian Redmond <chzbrgr71@users.noreply.github.com>
2018-09-12 14:52:14 -0700

    SP needs contributor rights
    this is necessary for the Jenkins lab later where the builds run as this context

--- labs/build-application/reg-acr.sh
+++ labs/build-application/reg-acr.sh
@@ -12,4 +12,4 @@ CLIENT_ID=$(az aks show --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER
 ACR_ID=$(az acr show --name $ACR_NAME --resource-group $ACR_RESOURCE_GROUP --query "id" --output tsv)
 
 # Create role assignment
-az role assignment create --assignee $CLIENT_ID --role Reader --scope $ACR_ID
+az role assignment create --assignee $CLIENT_ID --role Contributor --scope $ACR_ID
